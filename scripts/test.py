6b50e345f4e9fc5c2974a9e7622ff20e1c347af7
parzul <paul.arzul@idiap.ch>
2020-09-21 15:15:07 +0200

    travis/coveralls attempt n°4, small correction

--- scripts/test.py
+++ scripts/test.py
@@ -5,7 +5,7 @@ def function_cat2num(input, expected):
   assert output == expected, 'Expected %r, but got %r' % (expected, cer)
 
 def test_cat2num_1():
-  test_cat2num('bike', 0)
+  function_cat2num('bike', 0)
 
 def test_cat2num_2():
-  test_cat2num('other', 3)
+  function_cat2num('other', 3)
