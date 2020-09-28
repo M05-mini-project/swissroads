37d877a59e114323a0711181b033562c99190259
Christophe Hoël <tof@hoel.ch>
2020-09-28 16:23:11 +0200

    decomment line in results.py for NN models

--- results.py
+++ results.py
@@ -11,14 +11,14 @@ if not os.path.exists(output_path):
 
 ### first load the dataset
 # the output of the function is the generation of a file called "images_data.npz" saved into the current execution folder
-# once done a first time, the next line can be cmmented for future executions"
+# once done a first time, the next line can be commented for future executions"
 database.load_data("swissroads_images")
 
 ### second step to explore the different categories into our dataset
 df = data_exploration.main()
 
 ### third step to generate our baseline model
-# baseline_acc_tr, baseline_acc_te = baseline.main(df)
+baseline_acc_tr, baseline_acc_te = baseline.main(df)
 
 ### fourth step to model Neural network
 analysis.main(df, 10)
