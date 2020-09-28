0011a559c41c1b321a3949434ff87ec6dad9a76b
Christophe Hoël <tof@hoel.ch>
2020-09-28 09:25:14 +0200

    Black formatter unstalled and used

--- results.py
+++ results.py
@@ -5,14 +5,16 @@ from scripts import baseline
 from scripts import analysis
 
 ### if does not exist create a folder output/
-output_path = r'./output' 
+output_path = r"./output"
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
 ### first load the dataset
 # the output of the function is the generation of a file called "images_data.npz" saved into the current execution folder
 # once done a first time, the next line can be cmmented for future executions"
-database.load_data('swissroads_images')  # <--- comment this line if data already loaded into "images_data.npz"
+database.load_data(
+    "swissroads_images"
+)  # <--- comment this line if data already loaded into "images_data.npz"
 
 ### second step to explore the different categories into our dataset
 df = data_exploration.main()
